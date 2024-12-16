974c84be4084962860044f592daa4ccdd31c6533
yashwanthgajji <gajjiyashwanth7@gmail.com>
2024-12-16 00:51:43 -0500

    home page implementation

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd'
+import moment from 'moment'
 
-const News = () => {
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+
+const { Text, Title } = Typography
+const { Option } = Select
+
+const News = ({ simplified }) => {
+
+  const { data: cryptoNews, isFetching } = useGetCryptoNewsQuery({newsCategory: 'Cryptocurrency', count: simplified ? 10 : 100})
   return (
-    <div>News</div>
+    <>
+
+    </>
   )
 }
 
