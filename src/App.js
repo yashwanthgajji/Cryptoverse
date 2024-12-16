0319c239716b9c54206c9031706862493fb77c1e
yashwanthgajji <gajjiyashwanth7@gmail.com>
2024-12-15 20:51:11 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -11,20 +11,28 @@ const App = () => {
         <Navbar/>
       </div>
       <div className='main'>
-      <Layout>
-        <div className="routes">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/exchanges" element={<Exchanges />} />
-            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
-            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
-            <Route path="/news" element={<News />} />
-          </Routes>
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className='footer'>
+          <Typography.Title level = {5} style={{color: 'white', textAlign: 'center'}}>
+            Cryptoverse <br/>
+            All Rights reserced
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
         </div>
-      </Layout>
-      </div>
-      <div className='footer'>
-
       </div>
     </div>
   )
